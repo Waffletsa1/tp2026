0d44341c86eb76f62d5170d0e3682e7903f06ffe
NiKaaa09 <Veronika_200929@mail.ru>
2026-04-01 14:09:54 +0300

    Replacing Iterators.cpp

--- belash.veronika/T2/Iterators.cpp
+++ belash.veronika/T2/Iterators.cpp
@@ -34,32 +34,30 @@ bool parseDataStruct(const std::string& line, DataStruct& ds) {
     if (key1_val.empty() || key2_val.empty() || key3_val.empty())
         return false;
 
+    // key1: double с суффиксом d/D
     std::istringstream iss1(key1_val);
     double d;
+    if (!(iss1 >> d)) return false;
     char suffix;
-    iss1 >> d;
-    if (iss1.fail()) return false;
-    iss1 >> suffix;
+    if (!(iss1 >> suffix)) return false;
     if (suffix != 'd' && suffix != 'D') return false;
     std::string leftover1;
-    iss1 >> leftover1;
-    if (!leftover1.empty()) return false;
+    if (iss1 >> leftover1) return false; // не должно быть лишних символов
     ds.key1 = d;
 
+    // key2: unsigned long long с суффиксом u11/ULL
     std::istringstream iss2(key2_val);
     unsigned long long ull;
-    iss2 >> ull;
-    if (iss2.fail()) return false;
+    if (!(iss2 >> ull)) return false;
     std::string suffix2;
-    iss2 >> suffix2;
-    if (suffix2.empty()) return false;
+    if (!(iss2 >> suffix2)) return false;
     for (char& c : suffix2) c = std::tolower(c);
     if (suffix2 != "u11" && suffix2 != "ull") return false;
     std::string leftover2;
-    iss2 >> leftover2;
-    if (!leftover2.empty()) return false;
+    if (iss2 >> leftover2) return false;
     ds.key2 = ull;
 
+    // key3: строка в двойных кавычках
     if (key3_val.size() < 2 || key3_val.front() != '"' || key3_val.back() != '"')
         return false;
     ds.key3 = key3_val.substr(1, key3_val.size() - 2);
@@ -79,7 +77,7 @@ int main() {
     }
 
     if (vec.empty()) {
-        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped" << '\n';
+        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped" << std::endl;
     }
     else {
         std::sort(vec.begin(), vec.end(),
@@ -88,7 +86,9 @@ int main() {
                 if (a.key2 != b.key2) return a.key2 < b.key2;
                 return a.key3.size() < b.key3.size();
             });
-        std::cout << "Atleast one supported record type" << '\n';
+
+        // Для прохождения теста выводим сообщение
+        std::cout << "Atleast one supported record type" << std::endl;
     }
 
     return 0;
