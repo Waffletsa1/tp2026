e15f5803fee78385d88606fa77c41edacdcda615
NiKaaa09 <Veronika_200929@mail.ru>
2026-04-03 10:33:26 +0300

    Replacing Iterators.cpp

--- belash.veronika/T2/Iterators.cpp
+++ belash.veronika/T2/Iterators.cpp
@@ -42,135 +42,86 @@ private:
     std::basic_ios<char>::fmtflags fmt_;
 };
 
-std::string extractValue(const std::string& line, const std::string& key)
-{
-    std::string search = ":" + key + " ";
-    size_t pos = line.find(search);
-    if (pos == std::string::npos)
-    {
-        return "";
-    }
-    pos += search.length();
-
-    if (pos < line.size() && line[pos] == '"')
-    {
-        size_t end = line.find('"', pos + 1);
-        if (end == std::string::npos)
-        {
-            return "";
-        }
-        return line.substr(pos, end - pos + 1);
-    }
-    else
-    {
-        size_t end = line.find(':', pos);
-        if (end == std::string::npos)
-        {
-            return "";
-        }
-        return line.substr(pos, end - pos);
-    }
-}
-
 std::istream& operator>>(std::istream& in, DataStruct& dest)
 {
     std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-        return in;
-    }
+    if (!sentry) return in;
 
     std::string line;
     while (std::getline(in, line))
     {
-        if (!line.empty() && line.back() == '\r')
+        if (!line.empty() && line.back() == '\r') line.pop_back();
+        if (line.size() < 4 || line[0] != '(' || line[1] != ':' ||
+            line[line.size() - 2] != ':' || line[line.size() - 1] != ')')
+            continue;
+
+        std::istringstream iss(line);
+        char dummy;
+        iss >> dummy; // '('
+        iss >> dummy; // ':'
+
+        DataStruct temp;
+        bool f1 = false, f2 = false, f3 = false;
+        std::string key;
+        bool valid = true;
+
+        while (iss >> std::ws && !iss.eof() && valid)
         {
-            line.pop_back();
+            iss >> dummy; // читаем ':'
+            iss >> key;   // читаем имя поля
+
+            if (key == "key1" && !f1)
+            {
+                double val; char suf;
+                if (!(iss >> val >> suf) || (suf != 'd' && suf != 'D'))
+                {
+                    valid = false;
+                }
+                else
+                {
+                    temp.key1 = val;
+                    f1 = true;
+                }
+            }
+            else if (key == "key2" && !f2)
+            {
+                unsigned long long val; std::string suf;
+                if (!(iss >> val >> suf))
+                {
+                    valid = false;
+                }
+                else
+                {
+                    for (char& c : suf) c = std::tolower(static_cast<unsigned char>(c));
+                    if (suf != "ull") valid = false;
+                    else { temp.key2 = val; f2 = true; }
+                }
+            }
+            else if (key == "key3" && !f3)
+            {
+                char quote; std::string val;
+                if (!(iss >> quote) || quote != '"')
+                {
+                    valid = false;
+                }
+                else
+                {
+                    std::getline(iss, val, '"');
+                    temp.key3 = val;
+                    f3 = true;
+                }
+            }
+            else
+            {
+                valid = false;
+            }
         }
 
-        if (line.size() < 4)
+        if (valid && f1 && f2 && f3)
         {
-            continue;
+            dest = temp;
+            return in;
         }
-        if (line[0] != '(')
-        {
-            continue;
-        }
-        if (line[1] != ':')
-        {
-            continue;
-        }
-        if (line[line.size() - 2] != ':')
-        {
-            continue;
-        }
-        if (line[line.size() - 1] != ')')
-        {
-            continue;
-        }
-
-        std::string v1 = extractValue(line, "key1");
-        std::string v2 = extractValue(line, "key2");
-        std::string v3 = extractValue(line, "key3");
-
-        if (v1.empty() || v2.empty() || v3.empty())
-        {
-            continue;
-        }
-
-        std::istringstream iss1(v1);
-        double val1 = 0.0;
-        char s1 = ' ';
-        iss1 >> val1;
-        iss1 >> s1;
-
-        if (iss1.fail())
-        {
-            continue;
-        }
-        if (s1 != 'd' && s1 != 'D')
-        {
-            continue;
-        }
-
-        std::istringstream iss2(v2);
-        unsigned long long val2 = 0;
-        std::string s2;
-        iss2 >> val2;
-        iss2 >> s2;
-
-        if (iss2.fail())
-        {
-            continue;
-        }
-
-        for (size_t i = 0; i < s2.length(); ++i)
-        {
-            s2[i] = std::tolower(static_cast<unsigned char>(s2[i]));
-        }
-
-        if (s2 != "ull")
-        {
-            continue;
-        }
-
-        if (v3.size() < 2)
-        {
-            continue;
-        }
-        if (v3.front() != '"')
-        {
-            continue;
-        }
-        if (v3.back() != '"')
-        {
-            continue;
-        }
-
-        dest.key1 = val1;
-        dest.key2 = val2;
-        dest.key3 = v3.substr(1, v3.size() - 2);
-        return in;
     }
 
     in.setstate(std::ios::failbit);
