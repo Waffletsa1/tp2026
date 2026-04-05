5b952446e7f59d4750e78cb0bda197c9ee7fb218
Melnikova Anna <melnikova2.am@edu.spbstu.ru>
2026-04-05 23:13:48 +0300

    Fix final

--- melnikova.anna/T2/main.cpp
+++ melnikova.anna/T2/main.cpp
@@ -163,7 +163,7 @@ namespace nspace {
     std::ostream& operator<<(std::ostream& out, const DataStruct& src) {
         std::ostream::sentry sentry(out);
         if (!sentry) return out;
-        out << "(:key1 " << std::fixed << std::setprecision(10) << src.key1 << "d";
+        out << "(:key1 " << std::fixed << std::setprecision(1) << src.key1 << "d";
         out << ":key2 (:N " << src.key2.first << ":D " << src.key2.second << ":)";
         out << ":key3 \"" << src.key3 << "\":)";
         return out;
