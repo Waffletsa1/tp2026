34ae33515dc5f94f0d8964678f7b9aba142cd71b
EfimovDD06 <danafmv@gmail.com>
2026-04-20 18:08:09 +0300

    Fix T2

--- efimov.daniil/T2/main.cpp
+++ efimov.daniil/T2/main.cpp
@@ -1,4 +1,4 @@
-// 4 VAR
+﻿// 4 VAR
 // DBL SCI
 // CHR LIT
 #include <iostream>
@@ -11,6 +11,7 @@
 #include <cmath>
 #include <cctype>
 
+const double EPS = 1e-9;
 
 struct DataStruct
 {
@@ -18,7 +19,6 @@ struct DataStruct
     char key2;
     std::string key3;
 };
-
 
 struct DelimiterIO
 {
@@ -40,13 +40,6 @@ struct StringIO
     std::string& ref;
 };
 
-
-struct LabelIO
-{
-    std::string exp;
-};
-
-
 class Iofmtguard
 {
 public:
@@ -60,33 +53,13 @@ private:
     std::basic_ios<char>::fmtflags fmt_;
 };
 
-
 std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
 std::istream& operator>>(std::istream& in, DoubleSciIO&& dest);
 std::istream& operator>>(std::istream& in, CharLitIO&& dest);
 std::istream& operator>>(std::istream& in, StringIO&& dest);
-std::istream& operator>>(std::istream& in, LabelIO&& dest);
 std::istream& operator>>(std::istream& in, DataStruct& dest);
 std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
-
-
-bool compareDataStruct(const DataStruct& a, const DataStruct& b)
-{
-    const double EPS = 1e-9;
-
-    if (std::abs(a.key1 - b.key1) > EPS)
-    {
-        return a.key1 < b.key1;
-    }
-
-    if (a.key2 != b.key2)
-    {
-        return a.key2 < b.key2;
-    }
-
-    return a.key3.length() < b.key3.length();
-}
-
+bool compareDataStruct(const DataStruct& a, const DataStruct& b);
 
 int main()
 {
@@ -98,31 +71,16 @@ int main()
         if (input.empty()) continue;
         std::istringstream iss(input);
         DataStruct temp;
-        if (iss >> temp)
-        {
-            data.push_back(temp);
-        }
+        if (iss >> temp) data.push_back(temp);
     }
 
     std::sort(data.begin(), data.end(), compareDataStruct);
-
-    std::copy(
-        data.begin(),
-        data.end(),
-        std::ostream_iterator<DataStruct>(std::cout, "\n")
-    );
-
+    std::copy(data.begin(), data.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
     return 0;
 }
 
-
 Iofmtguard::Iofmtguard(std::basic_ios<char>& s)
-    : s_(s),
-    width_(s.width()),
-    fill_(s.fill()),
-    precision_(s.precision()),
-    fmt_(s.flags())
-{
+    : s_(s), width_(s.width()), fill_(s.fill()), precision_(s.precision()), fmt_(s.flags()) {
 }
 
 Iofmtguard::~Iofmtguard()
@@ -133,202 +91,154 @@ Iofmtguard::~Iofmtguard()
     s_.flags(fmt_);
 }
 
-
 std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
 {
     std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-        return in;
-    }
-    char c = '0';
+    if (!sentry) return in;
+    char c;
     in >> c;
-    if (in && (c != dest.exp))
-    {
-        in.setstate(std::ios::failbit);
-    }
+    if (in && c != dest.exp) in.setstate(std::ios::failbit);
     return in;
 }
 
+std::istream& operator>>(std::istream& in, CharLitIO&& dest)
+{
+    std::istream::sentry sentry(in);
+    if (!sentry) return in;
+    return in >> DelimiterIO{ '\'' } >> dest.ref >> DelimiterIO{ '\'' };
+}
+
+std::istream& operator>>(std::istream& in, StringIO&& dest)
+{
+    std::istream::sentry sentry(in);
+    if (!sentry) return in;
+    return std::getline(in >> DelimiterIO{ '"' }, dest.ref, '"');
+}
 
 std::istream& operator>>(std::istream& in, DoubleSciIO&& dest)
 {
     std::istream::sentry sentry(in);
     if (!sentry) return in;
 
-    std::string token;
-    in >> token;
+    if (in.peek() == '0')
+    {
+        std::streampos pos = in.tellg();
+        std::string test;
+        in >> test;
+        if (test == "0.0" || test == "0")
+        {
+            dest.ref = 0.0;
+            return in;
+        }
+        in.seekg(pos);
+    }
 
-    bool hasExponent = (token.find('e') != std::string::npos) ||
-        (token.find('E') != std::string::npos);
-    bool hasDot = token.find('.') != std::string::npos;
+    std::string num;
+    char sign = '+';
 
-    if (!hasExponent || !hasDot)
+    if (in.peek() == '-') in.get(sign);
+
+    char c;
+    while (in.get(c) && (std::isdigit(c) || c == '.')) num += c;
+
+    if (c != 'e' && c != 'E')
     {
         in.setstate(std::ios::failbit);
         return in;
     }
+    num += c;
 
-    try {
-        dest.ref = std::stod(token);
+    if (in.peek() == '+' || in.peek() == '-')
+    {
+        in.get(c);
+        num += c;
     }
-    catch (...) {
+    else
+    {
+        num += '+';
+    }
+
+    std::string exp;
+    while (in.get(c) && std::isdigit(c)) exp += c;
+    if (exp.empty())
+    {
         in.setstate(std::ios::failbit);
-    }
-
-    return in;
-}
-
-
-
-std::istream& operator>>(std::istream& in, CharLitIO&& dest)
-{
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
         return in;
     }
-    char c = '0';
-    in >> DelimiterIO{ '\'' } >> c >> DelimiterIO{ '\'' };
-    if (in)
-    {
-        dest.ref = c;
-    }
-    return in;
-}
+    if (c != std::char_traits<char>::eof()) in.putback(c);
+    num += exp;
 
-std::istream& operator>>(std::istream& in, StringIO&& dest)
-{
-    std::istream::sentry sentry(in);
-    if (!sentry)
+    try
     {
-        return in;
+        dest.ref = std::stod(num);
+        if (sign == '-') dest.ref = -dest.ref;
     }
-    return std::getline(in >> DelimiterIO{ '"' }, dest.ref, '"');
-}
-
-
-std::istream& operator>>(std::istream& in, LabelIO&& dest)
-{
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-        return in;
-    }
-    std::string data = "";
-    if ((in >> StringIO{ data }) && (data != dest.exp))
+    catch (...)
     {
         in.setstate(std::ios::failbit);
     }
     return in;
 }
-
 
 std::istream& operator>>(std::istream& in, DataStruct& dest)
 {
     std::istream::sentry sentry(in);
     if (!sentry) return in;
 
-    DataStruct input;
-    bool hasKey1 = false;
-    bool hasKey2 = false;
-    bool hasKey3 = false;
+    DataStruct temp;
+    bool f1 = false, f2 = false, f3 = false;
 
-    in >> DelimiterIO{ '(' } >> DelimiterIO{ ':' };
-
-    while (in && !(hasKey1 && hasKey2 && hasKey3))
+    if (!(in >> DelimiterIO{ '(' }))
     {
-        std::string key;
-        if (!(in >> LabelIO{ key })) break;
+        in.setstate(std::ios::failbit);
+        return in;
+    }
 
-        if (key == "key1")
+    for (size_t i = 0; i < 3; ++i)
+    {
+        if (!(in >> DelimiterIO{ ':' })) break;
+
+        std::string label;
+        if (!(in >> label)) break;
+
+        if (label == "key1")
         {
-            in >> DelimiterIO{ ' ' } >> DoubleSciIO{ input.key1 };
-            hasKey1 = true;
+            if (!(in >> DoubleSciIO{ temp.key1 })) break;
+            f1 = true;
         }
-        else if (key == "key2")
+        else if (label == "key2")
         {
-            in >> DelimiterIO{ ' ' } >> CharLitIO{ input.key2 };
-            hasKey2 = true;
+            if (!(in >> CharLitIO{ temp.key2 })) break;
+            f2 = true;
         }
-        else if (key == "key3")
+        else if (label == "key3")
         {
-            in >> DelimiterIO{ ' ' } >> StringIO{ input.key3 };
-            hasKey3 = true;
+            if (!(in >> StringIO{ temp.key3 })) break;
+            f3 = true;
         }
         else
         {
             in.setstate(std::ios::failbit);
-            return in;
+            break;
         }
-
-        in >> DelimiterIO{ ':' };
     }
 
-    in >> DelimiterIO{ ')' };
-
-    if (hasKey1 && hasKey2 && hasKey3)
-    {
-        dest = std::move(input);
-    }
-    else
+    if (!(in >> DelimiterIO{ ':' } >> DelimiterIO{ ')' }) || !f1 || !f2 || !f3)
     {
         in.setstate(std::ios::failbit);
+        return in;
     }
+
+    dest = std::move(temp);
     return in;
 }
 
-std::string formatDoubleSci(double value)
+bool compareDataStruct(const DataStruct& a, const DataStruct& b)
 {
-    if (value == 0.0)
-    {
-        return "0.0e+0";
-    }
-
-    double mantissa = value;
-    int exponent = 0;
-
-    if (std::abs(value) < 1.0)
-    {
-        while (std::abs(mantissa) < 1.0 && exponent > -100)
-        {
-            mantissa *= 10.0;
-            exponent--;
-        }
-    }
-    else
-    {
-        while (std::abs(mantissa) >= 10.0 && exponent < 100)
-        {
-            mantissa /= 10.0;
-            exponent++;
-        }
-    }
-
-    std::ostringstream oss;
-    oss << std::fixed << std::setprecision(1) << mantissa;
-    std::string mantissaStr = oss.str();
-
-    if (mantissaStr.back() == '0')
-    {
-        mantissaStr.pop_back();
-        if (mantissaStr.back() == '.')
-        {
-            mantissaStr.pop_back();
-        }
-    }
-
-    std::string result = mantissaStr + "e";
-    if (exponent >= 0) result += "+";
-    result += std::to_string(exponent);
-    for (char& c : result)
-    {
-        c = std::tolower(static_cast<unsigned char>(c));
-    }
-
-    return result;
+    if (std::abs(a.key1 - b.key1) > EPS) return a.key1 < b.key1;
+    if (a.key2 != b.key2) return a.key2 < b.key2;
+    return a.key3.length() < b.key3.length();
 }
-
 
 std::string formatCharLit(char c)
 {
@@ -346,15 +256,41 @@ std::string formatCharLit(char c)
 std::ostream& operator<<(std::ostream& out, const DataStruct& src)
 {
     std::ostream::sentry sentry(out);
-    if (!sentry)
-    {
-        return out;
-    }
+    if (!sentry) return out;
 
     Iofmtguard fmtguard(out);
 
-    out << "(:key1 " << formatDoubleSci(src.key1)
-        << ":key2 " << formatCharLit(src.key2)
+    double mantissa = src.key1;
+    int exponent = 0;
+
+    if (std::abs(mantissa) < EPS)
+    {
+        out << "(:key1 0.0e+00";
+    }
+    else
+    {
+        bool negative = mantissa < 0;
+        if (negative) mantissa = -mantissa;
+
+        while (mantissa < 1.0 && exponent > -100)
+        {
+            mantissa *= 10.0;
+            exponent--;
+        }
+        while (mantissa >= 10.0 && exponent < 100)
+        {
+            mantissa /= 10.0;
+            exponent++;
+        }
+
+        if (negative) mantissa = -mantissa;
+
+        out << "(:key1 " << std::fixed << std::setprecision(1) << mantissa;
+        out << "e";
+        if (exponent >= 0) out << "+";
+        out << exponent;
+    }
+    out << ":key2 " << formatCharLit(src.key2)
         << ":key3 \"" << src.key3 << "\":)";
     return out;
 }
